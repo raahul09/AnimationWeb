149d1dde70f1329d2bd7d0bfdb98923160a954ad
Rahul Patil <raahul09786@gmail.com>
2020-06-17 20:26:38 +0530

    Bootstrap work
    hand on practice work with bootstrap

---
+++ BootstrapWork/img/extScriptOpertor.js
@@ -0,0 +1,15 @@
+var Age = prompt("can you please enter your age /t");
+//var verify = (Age > 20) ? console.log("fuck its banana") : console.log("fdfnkd");
+
+if (Age <= 100) {
+    console.log("your less hard");
+
+    if (Age >= 18) {
+        console.log("party hard");
+    } else {
+        console.log("your teenager");
+    }
+} else {
+    console.log("your so hard");
+
+}
---
+++ BootstrapWork/img/jsfd.js
@@ -0,0 +1,20 @@
+var Name = 'Rahul';
+var dateOFBirth = '1-12-1996';
+var str = new String('test');
+
+console.log(str.length);
+console.log(str.valueOf());
+console.log(str.toString());
+
+console.log(str.toLowerCase());
+console.log(str.toLocaleUpperCase());
+console.log(str.toUpperCase());
+console.log(Name.toUpperCase());
+
+console.log(Name.trim('r'))
+
+// substr method
+
+var str = "google browser";
+console.log(str.substr(0, 5));
+console.log(str.substr(0, 10));
